b500c2d201a4d5eb33e46257fdce9951a3360466
Nicolas Fadul <nicolas.fadul@gmail.com>
2023-06-04 12:31:10 +0200

    Login_modifications: Login only for uninitialized useres, redirect otherwise. Cookie based session with time of 24h, ONLY ONE SESSION ALIVE. Save Sessions on SQL server

--- CreateDB/dbCreation.py
+++ CreateDB/dbCreation.py
@@ -1,14 +1,45 @@
 import mysql.connector 
+from app import db  
 
+# Connect to the database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="327baf2bcf1c1bc4ba3fbb5a9b95e69db7b1e61222e12c04bbd5e5a5d8a3676c"
+)
+
+
+my_cursor=mydb.cursor()
+my_cursor.execute("SHOW DATABASES")
+
+
+# Check if database already exists
+# else create it
+
+usersExist=False
+for db in my_cursor:
+    if(db[0]=="users"):
+        usersExist=True
+        print("Database already exists") 
+        
+if(usersExist==False):
+    my_cursor.execute("CREATE DATABASE users")
+    print("Database 'USERS' created")
+
+
+# Connect to the database
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="327baf2bcf1c1bc4ba3fbb5a9b95e69db7b1e61222e12c04bbd5e5a5d8a3676c",
+    database="users"
 )
 
-my_cursor=mydb.cursor()
 
-my_cursor.execute("CREATE DATABASE users")
-my_cursor.execute("SHOW DATABASES")
-for db in my_cursor:
-    print(db)
+
+for i in range(15):
+    add_employee = ("INSERT INTO Relays "
+               "(state, DateTime) "
+               "VALUES (%b, %d)")
+
+my_cursor.close()
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,flash
+from flask import Flask, render_template, request,flash,redirect,url_for,session
 
 # database
 from flask_sqlalchemy import SQLAlchemy
@@ -6,13 +6,17 @@ from flask_migrate import Migrate
 # datetime
 from datetime import datetime, timedelta
 
-# forms, need to create forms and 
-from flask_wtf import FlaskForm
-from wtforms import  SubmitField,PasswordField,EmailField,PasswordField,BooleanField,ValidationError
-from wtforms.validators import DataRequired,Email,EqualTo,Length
+# Custom forms for login and password and more... 
+from forms import PasswordForm,LogInForm,UserField
 
 #import to password hashing
 from werkzeug.security import generate_password_hash,check_password_hash
+
+# User Login
+from flask_login import UserMixin,LoginManager,login_user,login_required,logout_user,current_user
+
+# Forms
+from forms import PasswordForm,LogInForm,UserField
 
 
 # initiating Flask, bootstrap, CRTF key
@@ -23,13 +27,31 @@ app.config['SECRET_KEY'] = "t7w!z%C*F-JaNdRgUkXp2s5v8x/A?D(G+KbPeShVmYq3t6w9z$B&
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:327baf2bcf1c1bc4ba3fbb5a9b95e69db7b1e61222e12c04bbd5e5a5d8a3676c@localhost/users'
 db = SQLAlchemy(app)
 migrate=Migrate(app,db)
+ 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+ 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id)) 
+ 
+
+TIMETOWAIT=10
+
 
 #Model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(100), nullable=False,unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash= db.Column(db.String(128),nullable=False)
+    # One user can have many roles, referencing an inexisting column in ROles that will be created automatically.
+    # key_roles=db.relationship('Roles',backref='user',lazy='dynamic')
+    
+    # One to one relationship must have a role
+    roles_id = db.Column(db.Integer, db.ForeignKey('roles.id'),nullable=False,default=2)
+    
     # create a string
     def __repr__(self):
             return '<Email %r>' % self.email
@@ -42,50 +64,59 @@ class Users(db.Model):
     
     def checkPass(self,password):
        return check_password_hash(self.password_hash,password)
-   
-# Creation or updating
-class PasswordForm(FlaskForm):
-        email=EmailField("Email",validators=[Email()])
-        password_hash=PasswordField("Password",validators=[DataRequired(),EqualTo('passwordCheck',message='Passwords must match')])
-        passwordCheck=PasswordField("Confirm Password",validators=[DataRequired()])
-        submit = SubmitField('Submit')
-        
-# Create to check in
-class LogIn(FlaskForm):
-        email=EmailField("Email",validators=[Email()])
-        password_hash=PasswordField("Password",validators=[DataRequired()])
-        submit = SubmitField('Submit')
 
-# Create form class TESTUING
-class UserField(FlaskForm):
-        email=EmailField("Email",validators=[Email()])
-        submit = SubmitField('Submit')
+class Roles(db.Model, UserMixin):
+    id = db.Column(db.Integer,primary_key=True,unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    name = db.Column(db.String(100), nullable=False,unique=True)
+
+class Relay(db.Model, UserMixin):
+    _id= db.Column(db.Integer,primary_key=True,nullable=False,unique=True)
+    _state= db.Column(db.Boolean,nullable=False)
+    _DateTime= db.Column(db.DateTime,nullable=False)
+    
+    @property
+    def Relay(self):
+        raise AttributeError('Error')
+    
+    @Relay.setter
+    def state(self,state):
+        self._state=state
+        self._DateTime=datetime.now()
 
 # the methods that the request can accept
 
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def login(): 
     email = None
     password = None
-    form = LogIn()
-    LoginPass=None
+    form = LogInForm()
     CheckUser=None
+    
     #Validate form
     if form.validate_on_submit():
-        email = form.email.data
-        password = form.password_hash.data
+        users = Users.query.filter_by(email=form.email.data).first()
+        if users:
+            print("hash: " + form.password_hash.data)
+            if users.checkPass(form.password_hash.data):
+                session['user']=generate_password_hash(str(users.id))
+                login_user(users)
+                flash('Correct username and password')
+                return redirect(url_for('switch'))
         # clear data from form
-        form.email.data = ''
-        form.password_hash.data=''
-        CheckUser = Users.query.filter_by(email=email).first()
-        if(CheckUser):
-            LoginPass=check_password_hash(CheckUser.password_hash,password)
-        return render_template('index.html', email=email,form=form, password=password,CheckUser=CheckUser,LoginPass=LoginPass)
-
+            else:
+                flash('Invalid username or password')
+        else:
+            flash('Invalid username or password')
     return render_template('index.html', email=email,form=form, password=password,CheckUser=CheckUser)
 
+@app.route('/logout',methods=['POST','GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
-@app.route('/user/add', methods=['POST','GET'])
+@app.route('/user/signup', methods=['POST','GET'])
 def signup():
     email = None
     password = None
@@ -103,25 +134,31 @@ def signup():
         form.email.data = ''
         form.password_hash.data=''
         our_users= Users.query.order_by(Users.date_added + timedelta(hours=2))
-    return render_template('UserList.html', email=email, form=form, password=password, our_users=our_users)
+    return render_template('SignUp.html', email=email, form=form, password=password, our_users=our_users)
  
 @app.route('/user/update/<int:id>', methods=['POST','GET'])
 def update(id):
-    email = None
-    password = None
-    form = PasswordForm()
-    name_update=Users.query.get_or_404(id)
-    #Validate form
-    if form.validate_on_submit():
-        name_update.password_hash=generate_password_hash(form.password_hash.data,"sha256")
-        db.session.commit()
-        form.email.data = ''
-        form.password_hash.data = ''
-        flash("User modified successfully!")
-    return render_template('UpdateUser.html', email=email, form=form, password=password,our_user=name_update)
- 
-
+    print("USERID: ",current_user.id,", Role_ID: ",current_user.roles_id)
+    if(current_user.id == id or current_user.roles_id ==1):
+        email = None
+        password = None
+        form = PasswordForm()
+        name_update=Users.query.get_or_404(id)
+        #Validate form
+        if form.validate_on_submit():
+            name_update.password_hash=generate_password_hash(form.password_hash.data,"sha256")
+            db.session.commit()
+            form.email.data = ''
+            form.password_hash.data = ''
+            flash("User modified successfully!")
+        return render_template('UpdateUser.html', email=email, form=form, password=password,our_user=name_update)
+    flash("Not valid :S")
+    return redirect(url_for('signup'))
+    
+    
+    
 @app.route('/user/delete/<int:id>', methods=['POST','GET'])
+@login_required
 def delete(id):
     email = None
     password = None
@@ -144,11 +181,57 @@ def delete(id):
         our_users= Users.query.order_by(Users.date_added + timedelta(hours=2))
         return render_template('UserList.html', email=email, form=form, password=password,our_users=our_users)
      
- 
- 
+#TESTING
+@app.route('/switch', methods=['POST','GET'])
+@login_required
+def switch():
+    return render_template('Switch.html')
+    try:
+        db.session.pop('user',None)
+        del db.session['username']
+    except:
+        print("Error")
+    return render_template('logout.html')
+
+# database
+@app.route('/database', methods=['POST','GET'])
+def database():
+
+    if request.method=='POST':
+        db.drop_all()
+        db.create_all()
+    relays=Relay.query.all()
+    print(relays)
+    if(len(relays)==0):
+        print("No relays")
+        db.session.add(Relay(_id=1,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=2,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=3,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=4,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=5,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=6,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=7,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=8,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=9,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=10,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=11,_state=False,_DateTime=datetime.now()))
+        db.session.add(Relay(_id=12,_state=False,_DateTime=datetime.now()))
+        db.session.commit()
+    
+    roles=Roles.query.all()
+    print(roles)
+    if(len(roles)==0):
+        db.session.add(Roles(id=1,name='Admin'))
+        db.session.add(Roles(id=2,name='User'))
+        db.session.commit()
+    
+    
+    users=Users.query.order_by(Users.date_added + timedelta(hours=2))
+    return render_template('databse.html',our_roles=roles,our_relays=relays,our_users=users)
 
 #TESTING
 @app.route('/user', methods=['POST','GET'])
+@login_required
 def user():
     email = None
     form = UserField()
@@ -156,24 +239,67 @@ def user():
     if form.validate_on_submit():
         email = form.email.data
         form.email.data = ''
-    return render_template('user.html', email=email,form=form)
+    return render_template('userList.html', email=email,form=form)
 
 @app.route('/json', methods=['POST','GET'])
+@login_required
 def json():
-    json={"asd":"asd", "bb":"asd","sdasd":2}
-    return json
-    return render_template('user.html', email=email,form=form)
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        rely=Relay.query.filter_by(_id=request.json['id']).first()
+        print('IF: ',(datetime.now()-timedelta(seconds=TIMETOWAIT)) > rely._DateTime,', Delta time: ',rely._DateTime, ', Datetime to pass:',datetime.now()-timedelta(seconds=TIMETOWAIT))
+        if (datetime.now()-timedelta(seconds=TIMETOWAIT)) > rely._DateTime:
+            # changing the state of the relay
+            """
+            print(rely.state)
+            """
+            rely.state=request.json['value']
+            db.session.commit()
+        else:
+            return {"Error":"1","relay":str(request.json['id'])}
+
+        return {"Error":"0","relay":str(request.json['id'])}
+    if request.method == 'GET':
+        relays=Relay.copy()
+        for relay in relays:
+            time= str(int(TIMETOWAIT-datetime.now()-relay['_DateTime'].total_seconds()))
+        return relays
+
+    return Relay
+ 
+ 
+ 
+ # NEW TEMPLATES TO BE MADE
+
+# ------------------  NEW TEMPLATES TO BE MADE ------------------ #
+
+@app.route('/history', methods=['POST','GET'])
+@login_required
+def history():
+  return render_template('History.html')
 
 
-
+@app.route('/configuration', methods=['POST','GET'])
+@login_required
+def configuration():
+  return render_template('Configuration.html')
 
 @app.errorhandler(404)
 def error400(e):
     return render_template('error/404.html')  
 
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return redirect('error/404.html')
+
 @app.errorhandler(500)
 def error500(e):
     return render_template('error/500.html')  
 
+def init():
+    
+    db.session.add(Roles(role="Admin"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
