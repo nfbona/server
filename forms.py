6662500a7a865b25b447f799d736b8be2472b8bd
Nicolas Fadul <nicolas.fadul@gmail.com>
2023-07-20 22:58:10 +0200

    Libraries + docker

---
+++ forms.py
@@ -0,0 +1,24 @@
+
+# forms, need to create forms and 
+from flask_wtf import FlaskForm
+from wtforms import  SubmitField,PasswordField,EmailField,PasswordField,BooleanField,ValidationError,StringField
+from wtforms.validators import DataRequired,Email,EqualTo,Length
+
+
+# Creation or updating
+class PasswordForm(FlaskForm):
+        email=EmailField("Email",validators=[Email()])
+        password_hash=PasswordField("Password",validators=[DataRequired(),EqualTo('passwordCheck',message='Passwords must match')])
+        passwordCheck=PasswordField("Confirm Password",validators=[DataRequired()])
+        submit = SubmitField('Submit')
+        
+# Create to check in
+class LogInForm(FlaskForm):
+        email=EmailField("Email",validators=[Email()])
+        password_hash=PasswordField("Password",validators=[DataRequired()])
+        submit = SubmitField('Submit')
+
+# Create form class TESTUING
+class UserField(FlaskForm):
+        email=EmailField("Email",validators=[Email()])
+        submit = SubmitField('Submit')
