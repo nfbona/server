84de403ddc9e8a0d760b3d0419cdadaa8ea02bcf
Nicolas Fadul <nicolas.fadul@gmail.com>
2023-08-26 01:44:31 +0200

    New module Schedule

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from werkzeug.security import generate_password_hash
 # User Login
 from flask_login import LoginManager,login_user,login_required,logout_user,current_user
 
+
 # ----------REQUEST COOKIE -----------
 """
 Response.set_cookie('key','value')
