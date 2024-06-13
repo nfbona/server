b71be27b86387ade02079a4dbe9b68bc3da9e6a7
Nicolas Fadul <nicolas.fadul@gmail.com>
2024-06-13 17:43:31 +0200

    Migration1: GPIO modification

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
 pins = [2, 3, 4, 17, 27, 22, 10, 9]  # Add more pin numbers as needed
 
 # Set up SQLAlchemy engine and session
